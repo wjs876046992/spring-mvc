5c3064f87924014700a43e13a70ade3cfc5407fb
herman.wu <hermanwu@augmentum.com.cn>
2017-05-26 15:29:21 +0800

    resolve cors request

--- src/main/java/com/herman/controller/UserController.java
+++ src/main/java/com/herman/controller/UserController.java
@@ -3,30 +3,40 @@ package com.herman.controller;
 import com.herman.entity.User;
 import com.herman.service.impl.UserService;
 import org.apache.log4j.Logger;
-import org.apache.log4j.lf5.viewer.LogFactor5Dialog;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("api/user")
 public class UserController {
 
     private static Logger log = Logger.getLogger(UserController.class);
     @Resource
     private UserService userService;
 
+//    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+//    public String list(@PathVariable Integer id, Model model) {
+//        log.error("user id : " + id);
+//        User user = this.userService.findUserById(id);
+//        model.addAttribute("user", user);
+//        return "userList";
+//    }
+
+    @ResponseBody
     @RequestMapping(value="/{id}", method = RequestMethod.GET)
-    public String list(@PathVariable Integer id, Model model) {
-        log.error("user id : " + id);
+    public Map<String, Object> findUser(@PathVariable Integer id) {
+        Map<String, Object> result = new HashMap<String, Object>();
         User user = this.userService.findUserById(id);
-        model.addAttribute("user", user);
-        return "userList";
+        result.put("user", user);
+        return result;
     }
 }
